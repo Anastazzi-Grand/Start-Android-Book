d0e2a237d2c8756ed099ba5fe706a38a5f198154
Anastazzi-Grand <vnedostupa@yandex.ru>
2020-07-30 23:17:16 +0300

    Countries. Finished main class

--- Countries001/app/src/main/java/ru/itmo/countries001/MainActivity.java
+++ Countries001/app/src/main/java/ru/itmo/countries001/MainActivity.java
@@ -16,13 +16,29 @@ public class MainActivity extends AppCompatActivity {
     int[] embl = {R.drawable.israel1, R.drawable.usa1, R.drawable.russia1, R.drawable.germany1, R.drawable.poland1, R.drawable.france1};
     int[] flag = {R.drawable.israel2, R.drawable.usa2, R.drawable.russia2, R.drawable.germany2, R.drawable.poland2, R.drawable.france2};
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btns[0] = findViewById(R.id.button1);
+        btns[1] = findViewById(R.id.button2);
+        btns[2] = findViewById(R.id.button3);
+        btns[3] = findViewById(R.id.button4);
+        btns[4] = findViewById(R.id.button5);
+        btns[5] = findViewById(R.id.button6);
+        ivEmbl = findViewById(R.id.ivEmbl);
+        ivFlag = findViewById(R.id.ivFlag);
+        tvName = findViewById(R.id.tvName);
     }
 
     public void goPict(View view) {
+        for (int i = 0; i < flag.length; i++) {
+            if (view == btns[i]) {
+                ivEmbl.setImageResource(embl[i]);
+                ivFlag.setImageResource(flag[i]);
+                tvName.setText(btns[i].getText().toString());
+            }
+        }
     }
 }
