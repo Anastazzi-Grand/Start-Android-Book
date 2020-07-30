ffd7b61e4f825ee1286cb9773d1b364dcfcac71b
Anastazzi-Grand <vnedostupa@yandex.ru>
2020-07-30 22:39:40 +0300

    Countries. New methods

--- Countries001/app/src/main/java/ru/itmo/countries001/MainActivity.java
+++ Countries001/app/src/main/java/ru/itmo/countries001/MainActivity.java
@@ -3,12 +3,24 @@ package ru.itmo.countries001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button[] btns = new Button[6];
+    ImageView ivEmbl, ivFlag;
+    TextView tvName;
+    int[] embl = {R.drawable.israel1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goPict(View view) {
     }
 }
