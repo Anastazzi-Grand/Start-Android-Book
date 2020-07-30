59c7e5a78a0d00ad06ea7caf5a4e4f284d72ee49
Anastazzi-Grand <vnedostupa@yandex.ru>
2020-07-30 22:47:24 +0300

    Countries. Added emblems and flags to arrays

--- Countries001/app/src/main/java/ru/itmo/countries001/MainActivity.java
+++ Countries001/app/src/main/java/ru/itmo/countries001/MainActivity.java
@@ -13,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
     Button[] btns = new Button[6];
     ImageView ivEmbl, ivFlag;
     TextView tvName;
-    int[] embl = {R.drawable.israel1};
+    int[] embl = {R.drawable.israel1, R.drawable.usa1, R.drawable.russia1, R.drawable.germany1, R.drawable.poland1, R.drawable.france1};
+    int[] flag = {R.drawable.israel2, R.drawable.usa2, R.drawable.russia2, R.drawable.germany2, R.drawable.poland2, R.drawable.france2};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
